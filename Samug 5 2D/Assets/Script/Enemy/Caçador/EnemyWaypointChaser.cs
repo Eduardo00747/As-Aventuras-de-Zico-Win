01c66b20d8c5a4c2112221473c4aa391136fb992
Eduardo00747 <alcantaradesouzae@gmail.com>
2023-11-06 04:57:31 -0300

    V1.9
    Adicionado Score e logica da Jaula

--- Samug 5 2D/Assets/Script/Enemy/Caçador/EnemyWaypointChaser.cs
+++ Samug 5 2D/Assets/Script/Enemy/Caçador/EnemyWaypointChaser.cs
@@ -6,23 +6,27 @@ public class EnemyWaypointChaser : MonoBehaviour
 {
     public Transform target; // Referência ao transform do personagem
     public float moveSpeed = 2.0f; // Velocidade de movimento do inimigo
+    private Rigidbody2D rb; // Referência ao Rigidbody2D
 
     private SpriteRenderer spriteRenderer; // Referência ao Sprite Renderer
 
     //Variaveis de animação
     private Animator animator;
+    private bool canMove = true; // Variável para controlar se o inimigo pode se mover
+
 
     void Start()
     {
         // Obtém a referência ao SpriteRenderer
         spriteRenderer = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>(); // Obtém a referência ao Rigidbody2D
 
         animator = GetComponent<Animator>();
     }
 
     void Update()
     {
-        if (target != null)
+        if (canMove && target != null)
         {
             // Calcula a direção em direção ao personagem
             Vector3 direction = (target.position - transform.position).normalized;
@@ -31,10 +35,35 @@ public class EnemyWaypointChaser : MonoBehaviour
 
             // Movimenta o inimigo na direção horizontal
             Vector3 movement = new Vector3(direction.x, 0, 0) * moveSpeed * Time.deltaTime;
+            // Congela a posição Y do Rigidbody
+            rb.constraints = RigidbodyConstraints2D.FreezePositionY | RigidbodyConstraints2D.FreezeRotation; 
+
             transform.Translate(movement);
 
             // Define FlipX com base na direção do movimento
             spriteRenderer.flipX = (direction.x < 0);
         }
     }
+
+    // Quando o inimigo colide com algo
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Verifica se o objeto tem a tag "Gaiola"
+        if (collision.collider.CompareTag("Gaiola"))
+        {
+            // Define a variável para parar o movimento
+            canMove = false;
+            animator.SetBool("isRun", false);
+
+            // Inicia uma função que reativará o movimento após 3 segundos
+            Invoke("ResumeMovement", 3f);
+        }
+    }
+
+    // Função para retomar o movimento
+    private void ResumeMovement()
+    {
+        canMove = true;
+    }
+
 }
